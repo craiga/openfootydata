417db5760668dd449fd5fa4bb16caae81a08f443
Craig Anderson <craiga@craiga.id.au>
2016-05-23 20:48:08 +1000

    Make sorting work

--- openfootydata/settings.py
+++ openfootydata/settings.py
@@ -113,7 +113,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    )
 }
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
